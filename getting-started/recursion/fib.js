4f686e328ec767140f4580a135faf0e0e6b52907
Julian Buchholz <julian.buchholz@jimdo.com>
2023-01-18 16:16:37 +0100

    add: fibonachi

--- getting-started/recursion/fib.js
+++ getting-started/recursion/fib.js
@@ -21,6 +21,7 @@ const memoize = (func) => {
 const memoizedFibonacci = memoize(fib);
 
 const cacheFib = new Map();
+const cacheFibOld = new Map()
 
 function fibCache(n) {
   if (n === 0 || n === 1) {
@@ -29,21 +30,38 @@ function fibCache(n) {
   if (cacheFib.has(n)) {
     return cacheFib.get(n);
   }
-  cacheFib.set(n, fibCache(n - 1) + fibCache(n - 2));
-  return fibCache(n - 1) + fibCache(n - 2);
+  const result = fibCache(n - 1) + fibCache(n - 2);
+  cacheFib.set(n, result);
+  return result;
 }
+
+function fibCacheOld(n) {
+  if (n === 0 || n === 1) {
+    return n;
+  }
+  if (cacheFibOld.has(n)) {
+    return cacheFibOld.get(n);
+  }
+  cacheFibOld.set(n, fibCacheOld(n - 1) + fibCacheOld(n - 2));
+  return fibCacheOld(n - 1) + fibCacheOld(n - 2);
+}
+
+console.time("CacheOld");
+//fibCache(30);
+fibCacheOld(30);
+console.timeEnd("CacheOld");
+
+console.time("Cache");
+//fibCache(30);
+fibCache(30);
+console.timeEnd("Cache");
 
 console.time("Memoized");
 //memoizedFibonacci(30);
-console.log(memoizedFibonacci(30));
+memoizedFibonacci(30);
 console.timeEnd("Memoized");
 
 /* console.time("Regular");
 //fib(30);
 console.log(fib(30));
 console.timeEnd("Regular"); */
-
-console.time("Cache");
-//fibCache(30);
-console.log(fibCache(30));
-console.timeEnd("Cache");
